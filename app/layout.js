7801c4735f43993e3b3d394e1913115d99e55d39
rubenmeire14-sudo <rubenmeire14@gmail.com>
2026-08-04 19:00:28 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Vinted Reseller Hub',
+  description: 'Webhook API en Dashboard',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="nl">
+      <body>{children}</body>
+    </html>
+  );
+}
